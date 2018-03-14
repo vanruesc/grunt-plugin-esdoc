56cf3c4884ca946a581056c47b48a4e05be3964b
Raoul v. R <vanruesc@outlook.de>
2018-03-14 13:56:12 +0100

    Iterate over the files array.
    This allows the user to specify multiple path pairs per task. Error handling has also been improved.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,61 @@ import esdoc from "esdoc";
 import path from "path";
 
 /**
+ * Generates documentation files for a given configuration.
+ *
+ * @private
+ * @param {Object} grunt - The Grunt instance.
+ * @param {Object} options - The ESDoc options.
+ */
+
+function generateDocumentation(grunt, options) {
+
+	if(options.source === null) {
+
+		grunt.fail.error("No source path specified");
+
+	} else if(!grunt.file.exists(options.source)) {
+
+		grunt.fail.error("Invalid source path");
+
+	} else if(options.destination === null) {
+
+		grunt.fail.error("No destination path specified");
+
+	} else {
+
+		try {
+
+			esdoc.generate(options);
+
+			// Read the coverage results and report them via the console.
+			const coveragePath = path.join(options.destination, "coverage.json");
+
+			// Don't report coverage if it's not available.
+			if(grunt.file.exists(coveragePath)) {
+
+				const coverageReport = grunt.file.readJSON(coveragePath);
+				const coverage = coverageReport.coverage;
+				const expected = coverageReport.expectCount;
+				const actual = coverageReport.actualCount;
+				const unsatisfactory = (Number.parseFloat(coverage.replace("%", "")) < options.coverageThreshold);
+
+				grunt.log[unsatisfactory ? "warn" : "ok"]("Coverage: " + coverage);
+				grunt.log[unsatisfactory ? "warn" : "ok"]("Files: " + actual + "/" + expected);
+
+			}
+
+		} catch(error) {
+
+			grunt.fail.fatal(error);
+
+		}
+
+	}
+
+}
+
+/**
  * Registers the ESDoc Grunt task.
  *
  * @param {Object} grunt - The Grunt instance.
@@ -36,49 +91,34 @@ export default function registerESDocTask(grunt) {
 
 		}
 
-		if(options.source === null) {
+		if(this.files.length > 0) {
 
-			grunt.fail.error("No source path specified");
+			this.files.forEach(function(file) {
 
-		} else if(options.destination === null) {
+				if(file.src !== undefined && file.src.length === 1) {
 
-			grunt.fail.error("No destination path specified");
-
-		} else {
-
-			try {
-
-				esdoc.generate(options);
-
-				// Read the coverage results and report them via the console.
-				const coveragePath = path.join(options.destination, "coverage.json");
-
-				// Don't report coverage if it isn't available.
-				if(grunt.file.exists(coveragePath)) {
-
-					const coverageReport = grunt.file.readJSON(coveragePath);
-					const coverage = coverageReport.coverage;
-					const expected = coverageReport.expectCount;
-					const actual = coverageReport.actualCount;
-					const unsatisfactory = (Number.parseFloat(coverage.replace("%", "")) < options.coverageThreshold);
-
-					grunt.log[unsatisfactory ? "warn" : "ok"]("Coverage: " + coverage);
-					grunt.log[unsatisfactory ? "warn" : "ok"]("Files: " + actual + "/" + expected);
+					options.source = file.src[0];
 
 				}
 
-			} catch(error) {
+				if(file.dest !== undefined) {
 
-				grunt.fail.fatal(error);
+					options.destination = file.dest;
 
-			} finally {
+				}
 
-				// Restore the log function.
-				console.log = log;
+				generateDocumentation(grunt, options);
 
-			}
+			});
+
+		} else {
+
+			generateDocumentation(grunt, options);
 
 		}
+
+		// Restore the log function.
+		console.log = log;
 
 	});
 
